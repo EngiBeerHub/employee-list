a0fb5ab6b348e4d20950633b73f2724c6e5f335e
EngiBeerHub <engibeer0422@gmail.com>
2023-04-20 00:49:10 +0900

    till show message log

---
+++ src/app/members/members.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Member } from '../member';
+import { MemberService } from '../member.service';
+
+@Component({
+  selector: 'app-members',
+  templateUrl: './members.component.html',
+  styleUrls: ['./members.component.css'],
+})
+export class MembersComponent implements OnInit {
+  members: Member[];
+  selectedMember: Member;
+
+  constructor(private MemberService: MemberService) {}
+
+  ngOnInit(): void {
+    this.getMembers();
+  }
+
+  onSelect(member: Member): void {
+    this.selectedMember = member;
+  }
+
+  getMembers(): void {
+    this.MemberService.getMembers() // Observable
+    .subscribe(members => this.members = members);
+  }
+}
